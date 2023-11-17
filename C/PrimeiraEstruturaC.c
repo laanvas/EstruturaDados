0eaa36a1705e4eacbf6384d298631a1fcd6a1ce4
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-17 09:29:42 -0300

    17-11-23

---
+++ C/PrimeiraEstruturaC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef struct{
+	int peso; //peso em quilogramas
+	int altura; //altura em centimentros
+} PesoAltura;
+
+PesoAltura pessoa1;
+pessoa1.peso = 80;
+pessoa1.altura = 185;
+
